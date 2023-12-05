675b1436e064dcf33a91c36babdbc1f93f2f8e56
Dev-ThalesRF <thalesr.freitas@gmail.com>
2023-12-05 17:51:18 -0300

    Create OP_Aritimeticos1.js

---
+++ OP_Aritimeticos1.js
@@ -0,0 +1,21 @@
+let idade = 30 // Declarando variavel
+console.log("Minha idade e:" + idade) // Imprimindo informação
+
+idade = 30 + 6 // operador aritmeticos soma
+console.log("Daqui 6 anos será: " + idade)
+
+idade = 30 - 6
+console.log("Há seis anos atras eu tinha: " + idade +" anos")
+
+let CodigoDoProduto_1023 = 1023
+let codigoExato = CodigoDoProduto_1023 - 1000
+console.log("Este eo produto com codigo: "+ codigoExato)
+
+let primeiroNumero = 1023
+let segundoNumero = 23
+console.log(primeiroNumero - segundoNumero)
+
+let PrecoProduto = 100.99
+let valorComTaxa = PrecoProduto * 2
+
+console.log("O valor com imposto é: R$" + valorComTaxa + " FAZ O L")
